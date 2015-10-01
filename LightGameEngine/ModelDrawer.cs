b3bbf2f1cf8385efbf384fdea5bd621b00f1a9d7
James Larsen <JamesB444@msn.com>
2015-10-01 00:12:55 -0600

    Made ModelDrawer static

--- LightGameEngine/ModelDrawer.cs
+++ LightGameEngine/ModelDrawer.cs
@@ -16,28 +16,15 @@ namespace LightGameEngine
 {
     class ModelDrawer
     {
-        private int textureId;
-        private ModelObject objectToDraw;
+        private ModelDrawer(){}
 
-        public ModelDrawer(Image image, ModelObject objectToDraw)
+        public static void Draw(ModelObject objectToDraw)
         {
-            GL.BindTexture(TextureTarget.Texture2D, textureId);
-            Bitmap bmp = new Bitmap(image);
-            BitmapData data = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
-            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgb, image.Height, image.Width, 0, OpenTK.Graphics.OpenGL.PixelFormat.Bgr, PixelType.Bitmap, data.Scan0);
-            GL.TexEnv(TextureEnvTarget.TextureEnv, TextureEnvParameter.TextureEnvMode, Color.Gray);
-            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.Clamp);
-            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.Clamp);
-        }
-
-        public void Draw()
-        {
-            GL.BindTexture(TextureTarget.Texture2D, this.textureId);
             GL.PushMatrix();
-            GL.Translate(this.objectToDraw.Position);
-            GL.Rotate(this.objectToDraw.ZRotation.Degrees, 0, 0, 1);
-            GL.Rotate(this.objectToDraw.XRotation.Degrees, 1, 0, 0);
-            GL.Rotate(this.objectToDraw.YRotation.Degrees, 0, 1, 0);
+            GL.Translate(objectToDraw.Position);
+            GL.Rotate(objectToDraw.ZRotation.Degrees, 0, 0, 1);
+            GL.Rotate(objectToDraw.XRotation.Degrees, 1, 0, 0);
+            GL.Rotate(objectToDraw.YRotation.Degrees, 0, 1, 0);
             foreach (Group g in objectToDraw.Groups)
             {
                 GL.Color3(g.Material.DiffuseColor.X,
@@ -49,8 +36,6 @@ namespace LightGameEngine
                     for (int i = 0; i < f.Count; i++)
                     {
                         var vertex = objectToDraw.Vertices[f[i].VertexIndex];
-                        var uv = objectToDraw.Textures[f[i].TextureIndex];
-                        GL.TexCoord2(uv.X, uv.Y);
                         GL.Vertex3(vertex.X, vertex.Y, vertex.Z);
                     }
                     GL.End();
